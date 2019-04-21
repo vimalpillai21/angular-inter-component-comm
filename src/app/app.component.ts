a8e0b673a09e96fb1fef32ed8625f53318491b7b
Vimal Pillai <vimalpillai@goodbits.in>
2019-04-21 20:40:38 +0530

    component property usage another comp

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type:'Linux Server',name:'Server1',content:'server for fun'},];
+  
+
+  
+}

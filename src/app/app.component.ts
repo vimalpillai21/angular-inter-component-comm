8694cc0722d83b3475d1a3ef11e1c61b6a3ccde8
Vimal Pillai <vimalpillai@goodbits.in>
2019-04-23 19:00:49 +0530

    Event Emitting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type:'Linux Server',name:'Server1',content:'server for fun'},];
   
+  onServerAdded(serverData:{serverName:string,serverContent:string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
   
 }

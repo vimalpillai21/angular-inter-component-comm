2613b1cea81a8743f7363f8267a4001ca3c361e4
Vimal Pillai <vimalpillai@goodbits.in>
2019-04-23 19:39:48 +0530

    input element reference ViewChild method

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output,ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,21 +9,23 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   // newServerName = '';
-  newServerContent = '';
-  
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput : ElementRef;
   constructor() { }
 
   ngOnInit() {
   }
 
   onAddServer(nameInput:HTMLInputElement) {
-    console.log(nameInput);
+  //  console.log(nameInput);
     this.serverCreated.emit({serverName:nameInput.value,
-      serverContent:this.newServerContent})
+      // serverContent:this.newServerContent})
+      serverContent:this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(nameInput:HTMLInputElement) {
     this.blueprintCreated.emit({serverName:nameInput.value,
-      serverContent:this.newServerContent})
+      // serverContent:this.newServerContent})
+      serverContent:this.serverContentInput.nativeElement.value});
   }
 }
